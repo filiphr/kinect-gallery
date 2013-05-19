bb7cd35892e6e0da159ea02ac8dd233539019b59
filip.hrisafov@gmail.com <filip.hrisafov@gmail.com@1fc741b6-163d-9420-f971-c0700d7d9282>
2013-05-19 18:21:20 +0000

    Adding the Kinect Gallery Project to SVN

---
+++ trunk/Kinect Gallery/Kinect Gallery/Helpers/ScrollHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Kinect_Gallery.Helpers
+{
+    class ScrollHelper
+    {
+        /// <summary>
+        /// Gets ScrollViewer from the dependency object
+        /// </summary>
+        /// <param name="o">Object from which you take the ScrollViewer</param>
+        /// <returns>ScrollViewer or null if there is none</returns>
+        public static DependencyObject GetScrollViewer(DependencyObject o)
+        {
+            // Return the DependencyObject if it is a ScrollViewer
+            if (o is ScrollViewer)
+            { return o; }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(o); i++)
+            {
+                var child = VisualTreeHelper.GetChild(o, i);
+
+                var result = GetScrollViewer(child);
+                if (result == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+    }
+}
