2bac1022a4ac0c022b76e41087a20b36c60ea253
filip.hrisafov@gmail.com <filip.hrisafov@gmail.com@1fc741b6-163d-9420-f971-c0700d7d9282>
2013-05-19 18:21:20 +0000

    Adding the Kinect Gallery Project to SVN

---
+++ Kinect Gallery/Kinect Gallery/Helpers/Folder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using Kinect_Gallery.Properties;
+
+
+namespace Kinect_Gallery.Helpers
+{
+    class Folder
+    {
+        public String FolderName { get; set; }
+        public String FolderPath { get; set; }
+        public String ImagePath { get; set; }
+
+        public Folder(String folderName, String folderPath, String imagePath)
+        {
+            this.FolderName = folderName;
+            this.FolderPath = folderPath;
+            this.ImagePath = imagePath;
+        }
+    }
+
+
+    class Folders : ObservableCollection<Folder>
+    {
+        public Folders()
+        {
+            String imagesPath = Settings.Default.ImagesPath;
+
+            Add(new Folder("BCN", @"C:\Users\test\Pictures\BCN", imagesPath+"\\folder.jpg"));
+            Add(new Folder("EBEC day 1", @"C:\Users\test\Pictures\EBEC day 1", imagesPath + "\\folder.jpg"));
+            Add(new Folder("Wallpapers", @"C:\Users\test\Pictures\Wallpapers", imagesPath + "\\folder.jpg"));
+            Add(new Folder("NG Belgrad", @"C:\Users\test\Pictures\NG Belgrad", imagesPath + "\\folder.jpg"));
+            Add(new Folder("Otvaranje", @"C:\Users\test\Pictures\EBEC 2012 da se sredat\Otvaranje", imagesPath + "\\folder.jpg"));
+            Add(new Folder("Team Design", @"C:\Users\test\Pictures\EBEC 2012 da se sredat\Team Design", imagesPath + "\\folder.jpg"));
+        }
+    }
+}
---
+++ Kinect Gallery/Kinect Gallery/Helpers/ImagePathConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace Kinect_Gallery.Helpers
+{
+    class ImagePathConverter : IValueConverter
+    {
+        private string imageDirectory = Directory.GetCurrentDirectory();
+        public string ImageDirectory
+        {
+            get { return imageDirectory; }
+            set { imageDirectory = value; }
+        }
+
+
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string imagePath = Path.Combine(ImageDirectory, (string)value);
+            return new BitmapImage(new Uri((string)value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
